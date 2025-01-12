e65b6f22b60004bb8d86afac9f26f3c5391f678e
Eduardo <eduardo009cs@gmail.com>
2025-01-11 19:03:38 -0600

    Se agrega footer con el link del código de la página en github

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return(
+        <a href="https://github.com/eduardo009cs/saad-app">
+            <i className="fa-brands fa-github"></i>
+        </a>
+    )
+}
+
+export default Footer;
